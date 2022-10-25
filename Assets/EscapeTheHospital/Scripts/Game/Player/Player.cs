14f9d6481e7cb9ff1eb1ffa1fb450b5fbf44f19f
buitin99 <96711097+buitin99@users.noreply.github.com>
2022-10-25 16:33:24 +0700

    up camera

---
+++ Assets/EscapeTheHospital/Scripts/Game/Player/Player.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using Cinemachine;
+
+public class Player : MonoBehaviour
+{
+    public Scripables scriptablePlayer;
+    public CinemachineFreeLook cmFL;
+    public GameObject positionSpawn;
+    private GameObject _player;
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        //Choice Player
+        _player = scriptablePlayer.scriptableObjects[0].obj;
+        //Choice Skin & Items
+        //Do Something
+        Vector3 positionPlayer = positionSpawn.transform.position;
+        GameObject _playerSpawn = Instantiate(_player,positionPlayer,Quaternion.identity);
+        cmFL.Follow = _playerSpawn.transform;
+        cmFL.LookAt = _playerSpawn.transform;    
+    }
+}
